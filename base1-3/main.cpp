f8034df0dd70de62ce3dd0203d0a5227d3da29e8
suchifan <sc@smail.nju.edu.cn>
2018-03-22 16:22:36 +0800

    first commit

---
+++ base1-3/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  base1-3
+//
+//  Created by suchao on 3/22/18.
+//  Copyright © 2018 cs.nju. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "input a & b & c\n";
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << "b^2-4ac=" << b*b-4*a*c << endl;
+    return 0;
+}
